08511a204d6430023593678064b238bb14b66125
vipullsingh <vipul.kr.vks@gmail.com>
2023-08-04 22:26:21 +0530

    'test'

---
+++ README.md
@@ -0,0 +1,33 @@
+# Tooltip Placement Example
+
+###### This is a simple example of how to use a tooltip in a React application and change its placement based on user interaction. The example uses the Material-UI library for the tooltip component and the useState hook for state management.
+
+## Features
+
+- The example renders four buttons, each with an onClick function that updates the placement state to the corresponding position.
+- The Tooltip component is rendered with the placement state as its placement prop.
+- When the user hover on the button, the tooltip appears with the placement set by the user.
+- The component uses Bootstrap classes for styling and positioning of the elements.
+
+## Getting Started
+
+- Install the dependencies by running npm install or yarn install.
+- Start the development server by running npm start or yarn start.
+- The example should now be running on http://localhost:3000.
+
+## Dependencies
+
+- React
+- Material-UI
+- Bootstrap
+
+## Tips
+
+- The example uses the Material-UI library, so be sure to have it installed and configured in your project.
+- The example uses Bootstrap classes for styling and positioning, so be sure to have it installed and configured in your project.
+- You can customize the background color by changing the background color in the css file.
+- You can customize the placement of the tooltip by changing the placement state.
+
+## Conclusion
+
+- This is a simple example of how to use a tooltip in a React application and change its placement based on user interaction. It should serve as a good starting point for your own project.
--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import "./styles.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState } from 'react';
+import Tooltip from "@mui/material/Tooltip";
 
-import { Navbar } from "./components/Navbar";
-import { Home } from "./pages/Home";
-import { List } from "./pages/List";
-import { Contact } from "./pages/Contact";
+const App = () => {
+  // state to keep track of tooltip placement
+  const [placement, setPlacement] = useState("top");
 
-export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navbar />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "/list", element: <List /> },
-        { path: "/contact", element: <Contact /> }
-      ]
-    }
-  ]);
+  // function to update placement state when button is clicked
+  const onBtnClick = (_, position) => {
+    setPlacement(position);
+  };
 
   return (
-    <div className="App">
-      <RouterProvider router={router} />
+    <div className="main container position-relative w-100 p-4">
+      <div className="d-flex h-100 justify-content-center align-items-start">
+        {/* 4 diffrent btn for setting tooltip in diffrent diffrent position, if button is selected then outline classs will be remove */}
+        <button type="button" onClick={(e) => onBtnClick(e, "top")} className={`btn btn-${placement === "top" ? "" : "outline-"}primary m-2`}>Top</button>
+        <button type="button" onClick={(e) => onBtnClick(e, "bottom")} className={`btn btn-${placement === "bottom" ? "" : "outline-"}primary m-2`}>Bottom</button>
+        <button type="button" onClick={(e) => onBtnClick(e, "left")} className={`btn btn-${placement === "left" ? "" : "outline-"}primary m-2`}>Left</button>
+        <button type="button" onClick={(e) => onBtnClick(e, "right")} className={`btn btn-${placement === "right" ? "" : "outline-"}primary m-2`}>Right</button>
+        {/* main class to center button and tooltip */}
+        <div className="position-absolute top-50 start-50 translate-middle-x">
+          <span className="position-relative">
+            {/* mui tooltip */}
+            <Tooltip title={placement} placement={placement} arrow>
+              {/* Hover button */}
+              <button type="button" className="btn btn-info btn-lg">Hover</button>
+            </Tooltip>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default App;
